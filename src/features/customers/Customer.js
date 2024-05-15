660c413e8949e0071f9b667d375ab78fea63a6a2
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-05-16 00:20:38 +0300

    connect redux app with react

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  const customer = useSelector((state) => state.customer);
+
+  return <h2>👋 Welcome, {customer.fullName}</h2>;
 }
 
 export default Customer;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App";
 
 import store from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
